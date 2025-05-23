0a2e568db4805535353ed04362e29004c86a1d4a
Nils Quetschlich <nils.quetschlich@tum.de>
2025-05-23 08:37:00 +0200

    💡 added copyrights

--- src/mqt/bench/targets/gatesets/ionq.py
+++ src/mqt/bench/targets/gatesets/ionq.py
@@ -6,6 +6,33 @@
 #
 # Licensed under the MIT License
 
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2018.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+# Copyright 2024 IonQ, Inc. (www.ionq.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 """Handles the available native gatesets for IonQ."""
 
 from __future__ import annotations
@@ -25,11 +52,13 @@ if TYPE_CHECKING:
 
 def get_ionq_forte_gateset() -> list[str]:
     """Returns the basis gates of the IonQ Forte gateset."""
+    add_ionq_equivalences()
     return ["rz", "gpi", "gpi2", "zz", "measure"]
 
 
 def get_ionq_aria_gateset() -> list[str]:
     """Returns the basis gates of the IonQ Aria gateset."""
+    add_ionq_equivalences()
     return ["rz", "gpi", "gpi2", "ms", "measure"]
 
 
