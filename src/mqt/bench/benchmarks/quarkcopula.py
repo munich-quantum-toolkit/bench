1945116d3cd1f402512c3a25703d55b9fb479220
Nils Quetschlich <nils.quetschlich@tum.de>
2025-05-28 10:32:23 +0200

    🎨 add a newline

--- src/mqt/bench/benchmarks/quarkcopula.py
+++ src/mqt/bench/benchmarks/quarkcopula.py
@@ -19,6 +19,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 """Copula circuit from the generative modeling application in QUARK framework. https://github.com/QUARK-framework/QUARK."""
 
 from __future__ import annotations
