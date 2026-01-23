f3d31316ad933959229e65397c6ecc4c96b3472b
Carla <carlareciofdz@gmail.com>
2026-01-23 16:21:01 +0100

    code errors

--- src/mqt/bench/benchmarks/iqpe_exact.py
+++ src/mqt/bench/benchmarks/iqpe_exact.py
@@ -33,17 +33,20 @@ def create_circuit(num_qubits: int) -> QuantumCircuit:
     # the eigenstate will be encoded, q[0] is the ancillary qubit where the phase
     # will be encoded. Only the ancillary qubit is measured, and the result will
     # be stored in "c", the classical register.
+    if num_qubits < 2:
+        raise ValueError("num_qubits must be >= 2 (1 ancilla + at least 1 phase bit)")
     num_qubits = num_qubits - 1  # because of ancilla qubit
-    q = QuantumRegister(2, "q")
+    q0 = QuantumRegister(1, "q0")
+    q1 = QuantumRegister(num_qubits, "q1")
     c = ClassicalRegister(num_qubits, "c")
-    qc = QuantumCircuit(q, c, name="iqpeexact")
+    qc = QuantumCircuit(q0, q1, c, name="iqpeexact")
 
     # Generate a random n-bit integer as a target phase "theta". The phase can be exactly
     # estimated
-    random.seed(10)
+    rng = random.seed(10)
     theta = 0
     while theta == 0:
-        theta = random.getrandbits(num_qubits)
+        theta = rng.getrandbits(num_qubits)
     lam = Fraction(0, 1)
     for i in range(num_qubits):
         if theta & (1 << (num_qubits - i - 1)):
@@ -53,13 +56,13 @@ def create_circuit(num_qubits: int) -> QuantumCircuit:
 
     # We apply an X gate to the q[1] qubit, to prepare the target qubit in the
     # |1> state
-    qc.x(q[1])
+    qc.x(q1[0])
 
     for k in range(num_qubits):
         index = num_qubits - 1 - k
         # We reset the ancillary qubit in order to reuse in each iteration
-        qc.reset(q[0])
-        qc.h(q[0])
+        qc.reset(q0[0])
+        qc.h(q0[0])
 
         # Controlled unitary. The angle is normalized from
         # [0, 2] to [-1, 1], which minimize the errors because uses shortest rotations
@@ -68,7 +71,7 @@ def create_circuit(num_qubits: int) -> QuantumCircuit:
             angle -= 2
 
         # We use pi convention for simplicity
-        qc.cp(angle * np.pi, q[0], q[1])
+        qc.cp(angle * np.pi, q0[0], q1[0])
 
         # We apply phase corrections based on previous measurements.
         for i in range(k):
@@ -76,10 +79,10 @@ def create_circuit(num_qubits: int) -> QuantumCircuit:
             true_angle = -1.0 / (1 << (k - i))
 
             with qc.if_test((c[m_index], 1)):
-                qc.p(true_angle * np.pi, q[0])
+                qc.p(true_angle * np.pi, q0[0])
 
-        qc.h(q[0])
+        qc.h(q0[0])
         # We measure and store the result for future corrections
-        qc.measure(q[0], c[k])
+        qc.measure(q0[0], c[index])
 
     return qc
