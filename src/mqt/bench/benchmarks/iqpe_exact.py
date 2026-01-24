79374b43416dd47413c1542d6823b596c0639993
Carla <carlareciofdz@gmail.com>
2026-01-24 12:10:02 +0100

    code errors

--- src/mqt/bench/benchmarks/iqpe_exact.py
+++ src/mqt/bench/benchmarks/iqpe_exact.py
@@ -14,7 +14,7 @@ import random
 from fractions import Fraction
 
 import numpy as np
-from qiskit.circuit import ClassicalRegister, QuantumCircuit, QuantumRegister
+from qiskit.circuit import ClassicalRegister, QuantumCircuit, QuantumRegister, IfElseOp
 
 from ._registry import register_benchmark
 
@@ -36,34 +36,31 @@ def create_circuit(num_qubits: int) -> QuantumCircuit:
     # be stored in "c", the classical register.
     if num_qubits < 2:
         raise ValueError("num_qubits must be >= 2 (1 ancilla + at least 1 phase bit)")
-    num_qubits = num_qubits - 1  # because of ancilla qubit
-    q0 = QuantumRegister(1, "q0")
-    q1 = QuantumRegister(num_qubits, "q1")
-    c = ClassicalRegister(num_qubits, "c")
-    qc = QuantumCircuit(q0, q1, c, name="iqpeexact")
+    num_bits = num_qubits - 1  # because of ancilla qubit
+    q = QuantumRegister(num_qubits, "q")
+    c = ClassicalRegister(num_bits, "c")
+    qc = QuantumCircuit(q, c, name="iqpeexact")
 
     # Generate a random n-bit integer as a target phase "theta". The phase can be exactly
     # estimated
-    rng = random.seed(10)
+    rng = random.Random(10)
     theta = 0
     while theta == 0:
-        theta = rng.getrandbits(num_qubits)
+        theta = rng.getrandbits(num_bits)
     lam = Fraction(0, 1)
-    for i in range(num_qubits):
-        if theta & (1 << (num_qubits - i - 1)):
+    for i in range(num_bits):
+        if theta & (1 << (num_bits - i - 1)):
             lam += Fraction(1, (1 << i))
-
-    # print(f"Debug: theta = {theta}, lam = {float(lam)}")
 
     # We apply an X gate to the q[1] qubit, to prepare the target qubit in the
     # |1> state
-    qc.x(q1[0])
+    qc.x(q[0])
 
-    for k in range(num_qubits):
-        index = num_qubits - 1 - k
+    for k in range(num_bits):
+        index = num_bits - 1 - k
         # We reset the ancillary qubit in order to reuse in each iteration
-        qc.reset(q0[0])
-        qc.h(q0[0])
+        qc.reset(q[0])
+        qc.h(q[0])
 
         # Controlled unitary. The angle is normalized from
         # [0, 2] to [-1, 1], which minimize the errors because uses shortest rotations
@@ -72,18 +69,18 @@ def create_circuit(num_qubits: int) -> QuantumCircuit:
             angle -= 2
 
         # We use pi convention for simplicity
-        qc.cp(angle * np.pi, q0[0], q1[0])
+        qc.cp(angle * np.pi, q[0], q[1])
 
         # We apply phase corrections based on previous measurements.
         for i in range(k):
-            m_index = num_qubits - 1 - i
+            m_index = num_bits - 1 - i
             true_angle = -1.0 / (1 << (k - i))
 
             with qc.if_test((c[m_index], 1)):
-                qc.p(true_angle * np.pi, q0[0])
+                qc.p(true_angle * np.pi, q[0])
 
-        qc.h(q0[0])
+        qc.h(q[0])
         # We measure and store the result for future corrections
-        qc.measure(q0[0], c[index])
+        qc.measure(q[0], c[index])
 
     return qc
