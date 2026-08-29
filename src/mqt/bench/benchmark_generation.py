feb9a152e107e548ff5492f374b84f6a06e68c2c
LAPTOP-N4TQ80AC\khusa <ekhusainov2003@gmail.com>
2026-08-29 13:14:48 +0200

    🚑️ Fix Sys Usage according to Daniels Haag PR

--- src/mqt/bench/benchmark_generation.py
+++ src/mqt/bench/benchmark_generation.py
@@ -19,14 +19,11 @@ from qiskit.circuit import ClassicalRegister, QuantumCircuit, SessionEquivalence
 from qiskit.compiler import transpile
 from qiskit.converters import circuit_to_dag
 from qiskit.transpiler import Layout, Target
+from typing_extensions import assert_never
 
 from .error_correction.shor_transpiler import ShorTranspiler
 from .error_correction.steane_transpiler import SteaneTranspiler
 
-if sys.version_info >= (3, 11):
-    from typing import Unpack
-else:
-    from typing_extensions import Unpack
 
 from .benchmarks import create_circuit
 from .targets.gatesets import get_target_for_gateset, ionq, rigetti
